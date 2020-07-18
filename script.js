c402b9afac7a8774d034a16b32838a7068bde9fd
Kamil <65686983+jastrzembowski@users.noreply.github.com>
2020-07-18 22:25:58 +0200

    script fixed

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "50";
+    document.getElementById("nav-menu").style.top = "50";
   } else {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("nav-menu").style.top = "0";
   }
 }
