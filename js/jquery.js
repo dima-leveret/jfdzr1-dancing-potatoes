cf237c604e46674c9eb7305724880d7698d20f22
Kamil Jastrzembowski <jastrzembowskikamil@gmail.com>
2020-08-24 20:48:01 +0200

    jsquery updated

--- js/jquery.js
+++ js/jquery.js
@@ -2,24 +2,24 @@ $(document).scroll(function(){
     var top = $(window).scrollTop();
     var find_class_small = $.contains('nav', '.small');
   
-    if(top > 50 && find_class_small == false) { // tu zmieniamy wysokosc - gdy strona zjedzie 50px w dol
-     $('nav').addClass('small'); // nawigacja otrzyma klase small
+    if(top > 620 && find_class_small == false) { 
+     $('nav').addClass('small'); 
     }
     else {
-     $('nav').removeClass('small'); // w przeciwnym wypadku usuwamy klase small
+     $('nav').removeClass('small'); 
     }
     
-    
+
   });
   $(document).scroll(function(){
     var pot = $(window).scrollTop();
     var find_class_font = $.contains('.font', '.small');
   
-    if(pot > 50 && find_class_font == false) { // tu zmieniamy wysokosc - gdy strona zjedzie 50px w dol
-     $('.font').addClass('small'); // nawigacja otrzyma klase small
+    if(pot > 50 && find_class_font == false) { 
+     $('.font').addClass('small'); 
     }
     else {
-     $('.font').removeClass('small'); // w przeciwnym wypadku usuwamy klase small
+     $('.font').removeClass('small');
     }
   
   });
